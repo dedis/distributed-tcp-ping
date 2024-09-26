e9b15f0df5618f2867cdbb5fb35d206b10a97229
PasinduTennage <pasindunivanthakatennage@gmail.com>
2024-09-26 19:14:29 +0200

    added stats to remote-test; all files checked

--- setup/remote_dummy.sh
+++ setup/remote_dummy.sh
@@ -4,6 +4,8 @@ pwd=$(pwd)
 rm nohup.out
 
 dummy_path="/dummy/dummy"
+stat_path="/dummy/stats"
+
 config="/home/${username}/dummy/dedis-config.yaml"
 
 local_output_path="logs/dummy/"
@@ -11,7 +13,7 @@ rm -r "${local_output_path}"; mkdir -p "${local_output_path}"
 
 for index in "${!replicas[@]}";
 do
-  sshpass ssh "${replicas[${index}]}"  -i ${cert}  "pkill -f dummy; pkill -f dummy"
+  sshpass ssh "${replicas[${index}]}"  -i ${cert}  "pkill -f dummy; pkill -f stats"
 done
 
 echo "Killed previously running instances"
@@ -26,11 +28,15 @@ nohup ssh ${replica5}  -i ${cert}   ".${dummy_path} --config ${config} --name 5 
 
 echo "Started dummy replicas"
 
+sleep 10
+
+nohup ssh ${replica6}  -i ${cert}   ".${stat_path} --config ${config}">"${local_output_path}"stats.log &
+
 sleep  120
 
 for index in "${!replicas[@]}";
 do
-  sshpass ssh "${replicas[${index}]}"  -i ${cert}  "pkill -f dummy; pkill -f dummy"
+  sshpass ssh "${replicas[${index}]}"  -i ${cert}  "pkill -f dummy; pkill -f stats"
 done
 
 echo "Killed dummy replicas"
