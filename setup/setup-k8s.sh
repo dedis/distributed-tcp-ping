adf0250d48ffe9a6c8904840f7c4a4c66a0e18b1
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-20 14:49:51 +0200

    k8s setup debug

--- setup/setup-k8s.sh
+++ setup/setup-k8s.sh
@@ -3,6 +3,10 @@ pwd="$(pwd)"
 . "${pwd}/setup/ip.sh"
 
 read -r -d '' cmd <<'EOF'
+# remove any stale kubernetes.list
+sudo rm -f /etc/apt/sources.list.d/kubernetes.list &&
+
+# add the Google keyring and repository for Focal
 sudo apt update &&
 sudo apt install -y apt-transport-https ca-certificates curl gpg &&
 sudo mkdir -p /etc/apt/keyrings &&
@@ -11,6 +15,8 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg \
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] \
   https://apt.kubernetes.io/ kubernetes-focal main" \
   | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
+
+# install Kubernetes components
 sudo apt update &&
 sudo apt install -y kubelet kubeadm kubectl &&
 sudo apt-mark hold kubelet kubeadm kubectl
