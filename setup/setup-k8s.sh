ebfde19bba72acf4be7baf761f827fb43c1e5e5a
Pasindu Tennage <pasindunivanthakatennage@gmail.com>
2025-06-20 14:39:12 +0200

    added k8s setup

---
+++ setup/setup-k8s.sh
@@ -0,0 +1,13 @@
+pwd=$(pwd)
+. "${pwd}"/setup/ip.sh
+
+cmd="sudo apt update; sudo apt install -y apt-transport-https ca-certificates curl gpg; sudo mkdir -p /etc/apt/keyrings; curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg; echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list ; sudo apt update ;sudo apt install -y kubelet kubeadm kubectl; sudo apt-mark hold kubelet kubeadm kubectl"
+
+
+
+for index in "${!replicas[@]}";
+do
+    sshpass ssh "${cmd}"
+done
+
+echo "setup complete"
